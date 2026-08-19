6758c92f78f4c62cd304a181e0cb9b286caa9c80
Aditya Deshmukh <adityadeshmukh14100@gmail.com>
2026-08-19 03:33:39 -0700

    Time: 4 ms (82.59%), Space: 46.9 MB (58.11%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,41 +1,32 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
 
-        List<Integer> list =  new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
 
-        for(int x : asteroids){
-            list.add(x);
-        }
+        for(int a : asteroids){
 
-        int i = 0;
-        while(i < list.size() - 1){
+            while(!st.isEmpty() && a<0 && st.peek()>0){
 
-            int a = list.get(i);
-            int b = list.get(i+1);
-
-            if(a > 0 && b < 0){
-                if(Math.abs(a) > Math.abs(b)){
-                    list.remove(i+1);
-                }else if(Math.abs(a) < Math.abs(b)){
-                    list.remove(i);
-
-                    if(i > 0) i--;
+                if(st.peek() < -a){
+                    st.pop();
+                }else if(st.peek() == -a){
+                    st.pop();
+                    a = 0;
+                    break;
                 }else{
-                    list.remove(i+1);
-                    list.remove(i);
-                    
-                    if(i > 0) i--;
+                    a = 0;
+                    break;
                 }
-            }else{
-                i++;
+
             }
+            if(a!=0) st.push(a);
         }
 
-        int ans[] = new int[list.size()];
+            int ans[] = new int[st.size()];
+            for(int j = st.size()-1; j>=0 ; j--){
+                ans[j] = st.pop();
+            }
 
-        for(int j=0 ; j<list.size() ; j++){
-            ans[j] = list.get(j);
-        }
-        return ans;
+            return ans;
     }
 }
